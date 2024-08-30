f14f2796c97230e8d5c8d7e48660d87fca49f17b
pandeyapp <pandeysakshi2211@gmail.com>
2024-08-30 12:00:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>THis is not the React Deafault page</code> and save to reload.
+          Edit <code>THis is sakshi's page</code> and save to reload.
         </p>
         <a
           className="App-link"
